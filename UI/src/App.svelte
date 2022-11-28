8c967e0b588a8cf999918d08855a7e91d0b31ad9
gustavo-bertoldi <gg.bertoldi@hotmail.com>
2022-11-28 15:09:31 +0000

    Added UI

---
+++ UI/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import GitHubLogo from './assets/github.svg';
+  import FileSelector from './lib/FileSelector.svelte';
+</script> 
+
+<main>
+  <h1>Cloud Computing - Map Reducer</h1>
+
+  <div class="card">
+    <FileSelector></FileSelector>
+  </div>
+
+  <p>
+    Source code <a href="https://github.com/gustavo-bertoldi/CC-MapReducer"><img src={GitHubLogo} alt="Source code"  target="_blank" rel="noreferrer" class="github-logo"/></a>
+  </p>
+</main>
+
+<style>
+  .github-logo {
+    width: 1.5em;
+    height: 1.5em;
+    vertical-align: middle;
+  }
+
+  .github-logo:hover {
+    color: white;
+  }
+</style>
---
+++ UI/src/lib/FileSelector.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { books, fetchFiles } from './bucketFetcher'
+
+    fetchFiles();
+  
+</script>
+  
+<div>
+    {#each $books as book}
+        <p>{book}</p>
+    {/each}
+</div>
+  
